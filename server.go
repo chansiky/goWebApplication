f2e47509396c83c9923a6c3c8d9f2ecb794a5cd3
chansiky <chansiky@gmail.com>
2018-10-25 00:03:05 -0500

    edit functionality

--- server.go
+++ server.go
@@ -2,16 +2,49 @@ package main
 
 import (
   "fmt"
+  "io/ioutil"
   "log"
   "net/http"
+  "html/template"
 )
 
-func handler(w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type Page struct {
+  Title string
+  Body  []byte
 }
 
-func main(){
-  http.HandleFunc("/", handler)
+func (p *Page) save() error {
+  filename := p.Title + ".txt"
+  return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+  filename := title + ".txt"
+  body, err := ioutil.ReadFile(filename)
+  if err != nil {
+    return nil, err
+  }
+  return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/view/"):]
+  p, _ := loadPage(title)
+  fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[len("/edit/"):]
+  p, err := loadPage(title)
+  if err != nil {
+    p = &Page{Title: title}
+  }
+  t, _ := template.ParseFiles("edit.html")
+  t.Execute(w, p)
+}
+
+func main() {
+  http.HandleFunc("/view/", viewHandler)
+  http.HandleFunc("/edit/", editHandler)
   log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
